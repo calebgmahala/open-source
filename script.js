8e0f8a15f7b816612fb4280fe2ea27886340e0d3
Caleb Mahala <31549512+calebgmahala@users.noreply.github.com>
2017-11-09 10:31:48 -0500

    swap

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
-var box = document.getElementById("index-ticketbox");
 var body = document.getElementsByTagName("body")[0];
-function move() {
+function indexMove() {
+	var box = document.getElementById("index-ticketbox");
+	if (window.innerWidth >= 768) {
+		var grey = document.getElementsByClassName("grey")[0].getBoundingClientRect();
+		if (body.scrollTop > 729 && grey.bottom > 345) {
+			box.style.opacity = "1";
+			box.style.position = "fixed";
+			box.style.top = "60px";
+			box.style.left = "90px";
+			box.style.margin = "0";
+		} else if (grey.bottom <= 345 && grey.bottom >= 285) {
+			var opacity = ((grey.bottom - 285)/6)/10;
+			var top = (60-(345 - grey.bottom))+"px";
+			box.style.position = "fixed";
+			box.style.top = top;
+			box.style.left = "90px";
+			box.style.margin = "0";
+			box.style.opacity = opacity
+		} else {
+			box.style.position = "static";
+			box.style.top = "0";
+			box.style.left = "0";
+			box.style.margin = "80px 0 0 90px";
+		}
+	}
+}
+function aboutMove() {
+	var box = document.getElementById("about-ticketbox");
 	if (window.innerWidth >= 768) {
 		var grey = document.getElementsByClassName("grey")[0].getBoundingClientRect();
 		if (body.scrollTop > 729 && grey.bottom > 345) {
@@ -29,4 +55,4 @@ function test() {
 	console.log(body.scrollTop);
 	console.log(document.getElementsByClassName("grey")[0].getBoundingClientRect());
 }
-setInterval (move, 1);
+setInterval (indexMove, 1);
