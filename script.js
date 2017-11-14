d7662afe1a53cb340a4045fceff98bdb306d005e
Caleb Mahala <31549512+calebgmahala@users.noreply.github.com>
2017-11-14 13:42:57 -0500

    finished

--- script.js
+++ script.js
@@ -1,107 +1,106 @@
-function move() {
+function index() {
 	if (window.innerWidth >= 768) {
-		if (window.location.href=="file:///Users/cgmahala171/Documents/assignments_cset/open-source/index.html") {
-			var tops = document.getElementsByTagName("header")[0].getBoundingClientRect();
-			var box = document.getElementsByClassName("tickets")[0];
-			var bottom = document.getElementsByClassName("grey")[0].getBoundingClientRect();
-			if (tops.bottom < (-544) && bottom.bottom > 345) {
-				box.classList.add("tickets-fixed");
-				box.style.top = "60px";
-				box.style.opacity = "1";
-			} else if (bottom.bottom <= 345 && bottom.bottom >= 285) {
-				box.classList.add("tickets-fixed");
-				var opacity = ((bottom.bottom - 285)/6)/10;
-				var top = (60-(345 - bottom.bottom))+"px";
-				box.style.top = top;
-				box.style.opacity = opacity;
-			} else {
-				box.classList.remove("tickets-fixed");
-				box.style.top = "0";
-				box.style.opacity = "1";
-			}
+		var tops = document.getElementsByTagName("header")[0].getBoundingClientRect();
+		var box = document.getElementsByClassName("tickets")[0];
+		var bottom = document.getElementsByClassName("grey")[0].getBoundingClientRect();
+		if (tops.bottom < (-544) && bottom.bottom > 345) {
+			box.classList.add("tickets-fixed");
+			box.style.top = "60px";
+			box.style.opacity = "1";
+		} else if (bottom.bottom <= 345 && bottom.bottom >= 285) {
+			box.classList.add("tickets-fixed");
+			var opacity = ((bottom.bottom - 285)/6)/10;
+			var top = (60-(345 - bottom.bottom))+"px";
+			box.style.top = top;
+			box.style.opacity = opacity;
+		} else {
+			box.classList.remove("tickets-fixed");
+			box.style.top = "0";
+			box.style.opacity = "1";
 		}
-
-		//-----------------------------------------
-
-		if (window.location.href=="file:///Users/cgmahala171/Documents/assignments_cset/open-source/about.html") {
-			var tops = document.getElementsByTagName("header")[0].getBoundingClientRect();
-			var box = document.getElementsByClassName("tickets")[0];
-			var bottom = document.getElementsByClassName("contact")[0].getBoundingClientRect();
-			if (tops.bottom < (-544) && bottom.bottom > 350) {
-				box.classList.add("tickets-fixed");
-				box.style.top = "60px";
-				box.style.opacity = "1";
-			} else if (bottom.bottom <= 350 && bottom.bottom >= 290) {
-				box.classList.add("tickets-fixed");
-				var opacity = ((bottom.bottom - 290)/6)/10;
-				var top = (60-(350 - bottom.bottom))+"px";
-				box.style.top = top;
-				box.style.opacity = opacity
-			} else {
-				box.classList.remove("tickets-fixed");
-				box.style.top = "0";
-				box.style.opacity = "1";
-			}
+		setInterval (index, 1);
+	}
+}
+function about() {
+	if (window.innerWidth >= 768) {
+		var tops = document.getElementsByTagName("header")[0].getBoundingClientRect();
+		var box = document.getElementsByClassName("tickets")[0];
+		var bottom = document.getElementsByClassName("contact")[0].getBoundingClientRect();
+		if (tops.bottom < (-544) && bottom.bottom > 350) {
+			box.classList.add("tickets-fixed");
+			box.style.top = "60px";
+			box.style.opacity = "1";
+		} else if (bottom.bottom <= 350 && bottom.bottom >= 290) {
+			box.classList.add("tickets-fixed");
+			var opacity = ((bottom.bottom - 290)/6)/10;
+			var top = (60-(350 - bottom.bottom))+"px";
+			box.style.top = top;
+			box.style.opacity = opacity
+		} else {
+			box.classList.remove("tickets-fixed");
+			box.style.top = "0";
+			box.style.opacity = "1";
 		}
-
-		//-----------------------------------------
-
-		if (window.location.href=="file:///Users/cgmahala171/Documents/assignments_cset/open-source/speakers.html") {
-			var tops = document.getElementsByTagName("header")[0].getBoundingClientRect();
-			var box = document.getElementsByClassName("tickets")[0];
-			var bottom = document.getElementsByClassName("afterparty")[0].getBoundingClientRect();
-			if (tops.bottom < (-544) && bottom.bottom > 300) {
-				box.classList.add("tickets-fixed");
-				box.style.top = "60px";
-				box.style.opacity = "1";
-			} else if (bottom.bottom <= 300 && bottom.bottom >= 240) {
-				box.classList.add("tickets-fixed");
-				var opacity = ((bottom.bottom - 240)/6)/10;
-				var top = (60-(300 - bottom.bottom))+"px";
-				box.style.top = top;
-				box.style.opacity = opacity
-			} else {
-				box.classList.remove("tickets-fixed");
-				box.style.top = "0";
-				box.style.opacity = "1";
-			}
+		setInterval (about, 1);
+	}
+}
+function speakers() {
+	if (window.innerWidth >= 768) {
+		var tops = document.getElementsByTagName("header")[0].getBoundingClientRect();
+		var box = document.getElementsByClassName("tickets")[0];
+		var bottom = document.getElementsByClassName("afterparty")[0].getBoundingClientRect();
+		if (tops.bottom < (-544) && bottom.bottom > 300) {
+			box.classList.add("tickets-fixed");
+			box.style.top = "60px";
+			box.style.opacity = "1";
+		} else if (bottom.bottom <= 300 && bottom.bottom >= 240) {
+			box.classList.add("tickets-fixed");
+			var opacity = ((bottom.bottom - 240)/6)/10;
+			var top = (60-(300 - bottom.bottom))+"px";
+			box.style.top = top;
+			box.style.opacity = opacity
+		} else {
+			box.classList.remove("tickets-fixed");
+			box.style.top = "0";
+			box.style.opacity = "1";
 		}
-
-		//-----------------------------------------
-
-		if (window.location.href=="file:///Users/cgmahala171/Documents/assignments_cset/open-source/sponsers.html") {
-			var tops = document.getElementsByTagName("header")[0].getBoundingClientRect();
-			var box = document.getElementsByClassName("tickets")[0];
-			var bottom = document.getElementsByClassName("table")[0].getBoundingClientRect();
-			var bottom2 = document.getElementsByClassName("sponsorship-details")[0].getBoundingClientRect();
-			if ((tops.bottom < (-544) && bottom.bottom > 780) || (bottom.bottom < -100 && bottom2.bottom > 300)) {
-				box.classList.add("tickets-fixed");
-				box.style.top = "60px";
-				box.style.opacity = "1";
-			} else if (bottom.bottom <= 780 && bottom.bottom >= 720) {
-				box.classList.add("tickets-fixed");
-				var opacity = ((bottom.bottom - 720)/6)/10;
-				var top = (60-(780 - bottom.bottom))+"px";
-				box.style.top = top;
-				box.style.opacity = opacity;
-			} else if (bottom.bottom > -100 && bottom.bottom < -40) {
-				box.classList.add("tickets-fixed");
-				var opacity = ((bottom.bottom + 100)/6)/10;
-				var top = (60+(100 + bottom.bottom))+"px";
-				box.style.top = top;
-				box.style.opacity = 1-opacity;
-			} else if (bottom2.bottom < 300 && bottom2.bottom > 240) {
-				box.classList.add("tickets-fixed");
-				var opacity = ((bottom2.bottom - 240)/6)/10;
-				var top = (60-(300 - bottom2.bottom))+"px";
-				box.style.top = top;
-				box.style.opacity = opacity;
-			} else {
-				box.classList.remove("tickets-fixed");
-				box.style.top = "0";
-				box.style.opacity = "1";
-			}
+		setInterval (speakers, 1);
+	}
+}
+function sponsers() {
+	if (window.innerWidth >= 768) {
+		var tops = document.getElementsByTagName("header")[0].getBoundingClientRect();
+		var box = document.getElementsByClassName("tickets")[0];
+		var bottom = document.getElementsByClassName("table")[0].getBoundingClientRect();
+		var bottom2 = document.getElementsByClassName("sponsorship-details")[0].getBoundingClientRect();
+		if ((tops.bottom < (-544) && bottom.bottom > 780) || (bottom.bottom < -100 && bottom2.bottom > 300)) {
+			box.classList.add("tickets-fixed");
+			box.style.top = "60px";
+			box.style.opacity = "1";
+		} else if (bottom.bottom <= 780 && bottom.bottom >= 720) {
+			box.classList.add("tickets-fixed");
+			var opacity = ((bottom.bottom - 720)/6)/10;
+			var top = (60-(780 - bottom.bottom))+"px";
+			box.style.top = top;
+			box.style.opacity = opacity;
+		} else if (bottom.bottom > -100 && bottom.bottom < -40) {
+			box.classList.add("tickets-fixed");
+			var opacity = ((bottom.bottom + 100)/6)/10;
+			var top = (60+(100 + bottom.bottom))+"px";
+			box.style.top = top;
+			box.style.opacity = 1-opacity;
+		} else if (bottom2.bottom < 300 && bottom2.bottom > 240) {
+			box.classList.add("tickets-fixed");
+			var opacity = ((bottom2.bottom - 240)/6)/10;
+			var top = (60-(300 - bottom2.bottom))+"px";
+			box.style.top = top;
+			box.style.opacity = opacity;
+		} else {
+			box.classList.remove("tickets-fixed");
+			box.style.top = "0";
+			box.style.opacity = "1";
 		}
+		setInterval (sponsers, 1);
 	}
 }
 function calculate() {
@@ -112,6 +111,7 @@ function calculate() {
 		var value = ((form1.value * 42) + parseInt(form2.value) + (form3.value * 10));
 		var out = document.getElementsByClassName("output")[0];
 		out.innerHTML = ("$ "+value);
+		setInterval (calculate, 1);
 	}
 }
 function test() {
@@ -119,5 +119,3 @@ function test() {
 	console.log(document.getElementsByTagName("header")[0].getBoundingClientRect());
 	console.log(document.getElementsByClassName("sponsorship-details")[0].getBoundingClientRect());
 }
-setInterval (move, 1);
-setInterval (calculate, 1);
